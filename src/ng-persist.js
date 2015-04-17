13a73bf5a57ec5cf3a32f33da5301ce713e344de
Alex Disler <AlexDisler@users.noreply.github.com>
2015-04-17 09:51:57 -0400

    Update ng-persist.js

--- src/ng-persist.js
+++ src/ng-persist.js
@@ -5,21 +5,84 @@
     angular.module('ng-persist', []);
 
     const $persist = ($q, $localStorage) => {
+        let isBrowser = false;
+        let isIos     = false;
+        let isAndroid = false;
+        if (!window.cordova && !window.device && !window.Keychain) {
+            isBrowser = true;
+        } else {
+            isAndroid = (window.device.platform === 'Android');
+            isIos     = (window.device.platform === 'iOS');
+        }
+
+        const writeToFile = (namespace, key, val) => {
+            var deferred = $q.defer();
+            window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory, (dir) => {
+                const filename = namespace + '.' + key + '.txt';
+                dir.getFile(filename, { create : true }, (file) => {
+                    if(!file) {
+                        deferred.reject();
+                    }
+                    file.createWriter((fileWriter) => {
+                        fileWriter.seek(fileWriter.length);
+                        var blob = new Blob([val], {type:'text/plain'});
+                        fileWriter.write(blob);
+                        deferred.resolve();
+                    }, (err) => {
+                        console.log(err);
+                        deferred.reject();
+                    });
+                });
+            });
+            return deferred.promise;
+        };
+
+        const readFromFile = (namespace, key) => {
+            var deferred = $q.defer();
+            const filename = namespace + '.' + key + '.txt';
+            window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory + filename, (fileEntry) => {
+                fileEntry.file((file) => {
+                    var reader = new FileReader();
+                    reader.onloadend = (evt) => {
+                        deferred.resolve(evt.target.result);
+                    };
+                    reader.readAsText(file);
+                });
+            }, (err) => {
+                console.log(err);
+                deferred.reject();
+            });
+            return deferred.promise;
+        };
+
         return {
             set(namespace = '', key = null, val = '') {
+                const saveToLocalStorage = () => {
+                    $localStorage[`${namespace}_${key}`] = val;
+                };
                 var deferred = $q.defer();
                 if (!key) {
                     deferred.reject(new Error('must specify a key'));
                 } else {
-                    if (window.cordova && window.Keychain) {
+                    if (isIos) {
                         var kc = new window.Keychain();
                         kc.setForKey(() => {
                                 deferred.resolve();
                             }, (err) => {
+                                console.log(err);
                                 deferred.reject(new Error('error saving to keychain'));
                             }, key, namespace, val);
+                    } else if (isAndroid) {
+                        writeToFile('activebuilding', 'sometest', 'this is my value')
+                            .then(() => {
+                                deferred.resolve();
+                            })
+                            .catch(() => {
+                                saveToLocalStorage();
+                                deferred.resolve();
+                            });
                     } else {
-                        $localStorage[`${namespace}_${key}`] = val;
+                        saveToLocalStorage();
                         deferred.resolve();
                     }
                 }
@@ -28,18 +91,33 @@
             get(namespace = '', key = null, fallback = '') {
                 var deferred = $q.defer();
                 let val = '';
+                const getFromLocalStorage = () => {
+                    return $localStorage[`${namespace}_${key}`];
+                };
                 if (!key) {
                     deferred.reject();
                 } else {
-                    if (window.cordova && window.Keychain) {
+                    if (isIos) {
                         var kc = new window.Keychain();
                         kc.getForKey((val) => {
                                 deferred.resolve(val);
                             }, (err) => {
+                                console.log(err);
                                 deferred.resolve(fallback);
                             }, key, namespace);
                     } else {
-                        val = $localStorage[`${namespace}_${key}`];
+                        if (isAndroid) {
+                            readFromFile('activebuilding', 'sometest')
+                                .then((val) => {
+                                    deferred.resolve(val);
+                                    console.log('AAA :: file read : ' + val);
+                                })
+                                .catch(() => {
+                                    val = getFromLocalStorage();
+                                });
+                        } else {
+                            val = getFromLocalStorage();
+                        }
                         if (val) {
                             deferred.resolve(val);
                         } else {
@@ -54,7 +132,7 @@
                 if (!key || !namespace) {
                     deferred.reject();
                 } else {
-                    if (window.cordova && window.Keychain) {
+                    if (isIos) {
                         var kc = new window.Keychain();
                         kc.removeForKey(() => {
                                 deferred.reject();
